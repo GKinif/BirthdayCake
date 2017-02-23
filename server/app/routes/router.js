fd462d5ba64594ac88bd35c4ad25e521919ca776
Guillaume Kinif <gki@emakina.com>
2017-02-23 22:49:39 +0100

    SERVER: removed setup route

--- server/app/routes/router.js
+++ server/app/routes/router.js
@@ -8,24 +8,6 @@ router.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:8080/api');
 });
 
-router.get('/setup', function(req, res) {
-
-    // create a sample user
-    var dude = new User({
-        name: 'The Dude',
-        password: 'password',
-        admin: true
-    });
-
-    // save the sample user
-    dude.save(function(err) {
-        if (err) throw err;
-
-        console.log('User saved successfully');
-        res.json({ success: true });
-    });
-});
-
 // apply the routes to our application with the prefix /api
 router.use('/api', userRoutes);
 
