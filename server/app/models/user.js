878c15c2614534ddbd5fff6aa643901a59d6cc94
Guillaume Kinif <gki@emakina.com>
2017-02-23 22:40:56 +0100

    SERVER: updating user model

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -4,7 +4,23 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('User', new Schema({
-    name: String,
-    password: String,
-    admin: Boolean
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    admin: Boolean,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    birthDate: { type: Date, required: true },
+    profilePic: String,
+    previousYears: [
+        {
+            year: Number,
+            cakeDate: Date,
+        }
+    ],
+    votes: [
+        {
+            dateCreation: Date,
+            author: { userId: String, firstName: String, lastName: String },
+        }
+    ],
 }));
