ac6d7d85db9ca8284425f3c724e53f4a6cfc5362
Guillaume Kinif <gki@emakina.com>
2017-02-23 17:34:06 +0100

    refactoring routes

---
+++ server/app/routes/userRoutes.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const config = require('../../config'); // get our config file
+const User   = require('../models/user'); // get our mongoose model
+
+// API ROUTES -------------------
+// get an instance of the router for api routes
+const userRoutes = express.Router();
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+userRoutes.post('/authenticate', function(req, res) {
+
+    // find the user
+    User.findOne({
+        name: req.body.name
+    }, function(err, user) {
+
+        if (err) throw err;
+
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign(user, config.secret, {
+                    expiresIn: 60 * 60 * 24 // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
+
+// route middleware to verify a token
+userRoutes.use(function(req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, config.secret, function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+});
+
+// route to show a random message (GET http://localhost:8080/api/)
+userRoutes.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+userRoutes.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+        res.json(users);
+    });
+});
+
+module.exports = userRoutes;
